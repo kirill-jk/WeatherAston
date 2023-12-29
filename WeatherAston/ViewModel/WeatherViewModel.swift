383269f059dfbfbcf82bc73fb558f8d9a09a07fe
kirill-jk <ikurilyuk4@gmail.com>
2023-12-29 15:07:55 +0300

    Project Commit

--- WeatherAston/ViewModel/WeatherViewModel.swift
+++ WeatherAston/ViewModel/WeatherViewModel.swift
@@ -5,4 +5,28 @@
 //  Created by Кирилл Курилюк on 29.12.2023.
 //
 
-import Foundation
+
+import UIKit
+import RxCocoa
+import RxSwift
+import CoreLocation
+
+final class WeatherViewModel {
+	
+	let weatherData = PublishRelay<WeatherModel>()
+	let weatherService = WeatherNetworkService()
+	let disposeBag = DisposeBag()
+
+	init() {
+		weatherService.weatherData.subscribe { event in
+			self.weatherData.accept(event)
+		}.disposed(by: disposeBag)
+	}
+	func getWeatherByCity(city: String) {
+		weatherService.fetchWeather(cityName: city)
+	}
+	
+	func getWeatherByLocation(lat: CLLocationDegrees, lon: CLLocationDegrees) {
+		weatherService.fetchWeather(latitude: lat, longitude: lon)
+	}
+}
